6449dd680096ce7125021314490c9205b705bc52
Chirath Fonseka <chirathfonseka98#gmail.com>
2022-10-16 15:01:42 +0530

    minor bug fixes in course controller class

--- src/main/java/com/mit/lms/controller/CourseController.java
+++ src/main/java/com/mit/lms/controller/CourseController.java
@@ -15,19 +15,19 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
-//    @GetMapping("/{lecturerId}")
-//    public List<Course> viewByLecId(String lecturerId){
-//        return courseService.viewByLecId (lecturerId);
-//    }
+    @GetMapping("/{lecturerId}")
+    public List<Course> viewByLecId(String lecturerId){
+        return courseService.viewByLecId (lecturerId);
+    }
 
     @PostMapping("/add")
     public Course addCourse(@RequestBody Course course){
         return courseService.addCourse (course);
     }
-    @GetMapping("/{courseId}")
-    public Optional<Course> viewById(@PathVariable String courseId){
-        return courseService.viewByCourseId (courseId);
-    }
+//    @GetMapping("/{courseId}")
+//    public Optional<Course> viewById(@PathVariable String courseId){
+//        return courseService.viewByCourseId (courseId);
+//    }
     @PutMapping("/update/{courseId}")
     public Course updateCourse(@RequestBody Course course, @PathVariable String courseId){
         return courseService.updateCourse (course, courseId);
--- src/main/java/com/mit/lms/repository/CourseRepository.java
+++ src/main/java/com/mit/lms/repository/CourseRepository.java
@@ -2,10 +2,12 @@ package com.mit.lms.repository;
 
 import com.mit.lms.model.Course;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface CourseRepository extends MongoRepository<Course, String> {
 
+    @Query("{lecturerId:'?0'}")
     List<Course> findByLecturerId(String lecturerId);
 }
