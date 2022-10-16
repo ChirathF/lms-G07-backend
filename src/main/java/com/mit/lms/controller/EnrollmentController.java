7c51306305c0d728e6b7750bf978f984c25bf4e4
aaruran <ladchumananaaruran@gmail.com>
2022-10-16 17:57:58 +0530

    Small changes

--- src/main/java/com/mit/lms/controller/EnrollmentController.java
+++ src/main/java/com/mit/lms/controller/EnrollmentController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/enrollment/")
+@RequestMapping("/enrollment")
 public class EnrollmentController {
 
     @Autowired
--- src/main/java/com/mit/lms/repository/EnrollmentRepository.java
+++ src/main/java/com/mit/lms/repository/EnrollmentRepository.java
@@ -2,10 +2,12 @@ package com.mit.lms.repository;
 
 import com.mit.lms.model.Enrollment;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface EnrollmentRepository extends MongoRepository<Enrollment, String> {
 
     boolean existsByUsernameAndCourseId(String userName, int courseId);
